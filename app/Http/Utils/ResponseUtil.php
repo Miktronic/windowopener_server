b2df51ae5581fc90893eb9c542005915c27c0462
shaheen2013 <coredeveloper.2013@gmail.com>
2023-03-17 18:02:55 +0600

    response key msg to message

--- app/Http/Utils/ResponseUtil.php
+++ app/Http/Utils/ResponseUtil.php
@@ -6,6 +6,6 @@ namespace App\Http\Utils;
 class ResponseUtil{
 
     public static function failedResponse($msg = 'Something went wrong! please try again.',$code=400){
-        return response()->json(['msg'=>$msg],$code);
+        return response()->json(['message'=>$msg],$code);
     }
 }
